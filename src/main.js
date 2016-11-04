e909e980377e9257e3dd39909b6fa210790afd9a
tushar-raj <tushar.raj@talentica.com>
2016-11-04 12:01:18 +0530

    cleaned up main.js

--- src/main.js
+++ src/main.js
@@ -2,14 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, Link, hashHistory } from 'react-router'
 
-import Counter from './Counter';
-import Countdown from './Countdown';
 import Home from './routes/home/components/HomeView';
 import AppContainer from './routes/app/AppContainer'
-import LeftPane from './routes/app/containers/LeftPane'
-import CenterPane from './routes/app/containers/CenterPane'
-import RightPane from './routes/app/containers/RightPane'
-
 
 document.addEventListener('DOMContentLoaded', function() {
     ReactDOM.render(
