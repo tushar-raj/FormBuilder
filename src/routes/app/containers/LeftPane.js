cf1bf9d8d08e9f11c9bcc3036901b35069617df3
tushar-raj <tushar.raj@talentica.com>
2016-11-07 12:32:45 +0530

    corrections and beautification

--- src/routes/app/containers/LeftPane.js
+++ src/routes/app/containers/LeftPane.js
@@ -6,23 +6,47 @@ import { ButtonGroup } from 'react-bootstrap';
 import LeftPaneItem from '../components/LeftPaneItem';
 import ItemTypes from '../../../constants/itemTypes';
 
-const basicImagePath = '../images/';
-const arrOfComponentLabels = [
-  {imagePath:basicImagePath+'textarea.png',componentTitle:'Text Area',name:''},
-  {imagePath:basicImagePath+'inputtext.png',componentTitle:'Input Text',name:''},
-  {imagePath:basicImagePath+'radio_button.png',componentTitle:'Radio Button Group',name:'RadioButtonGroup'},
-  {imagePath:basicImagePath+'checkbox.png',componentTitle:'Checkbox Group',name:''},
-  {imagePath:basicImagePath+'drop_down_list.png',componentTitle:'Dropdown',name:''},
-  {imagePath:basicImagePath+'button.jpg',componentTitle:'Button',name:''}
-];
-const componentLabelItems = arrOfComponentLabels.map((item,index) =>
-      /*
-       Ideally some unique id should be used for key,
-       for the time being index has been used,
-       this should be replaced once stable Ids are available
-      */
-      <LeftPaneItem key={index} imagePath={item.imagePath} componentTitle={item.componentTitle} name={item.name} />
-      );
+const basicImagePath = '../assets/';
+
+const arrOfComponentLabels = [{
+    imagePath: basicImagePath + 'textarea.png',
+    componentTitle: 'Text Area',
+    name: ''
+}, {
+    imagePath: basicImagePath + 'inputtext.png',
+    componentTitle: 'Input Text',
+    name: ''
+}, {
+    imagePath: basicImagePath + 'radio_button.png',
+    componentTitle: 'Radio Button Group',
+    name: 'RadioButtonGroup'
+}, {
+    imagePath: basicImagePath + 'checkbox.png',
+    componentTitle: 'Checkbox Group',
+    name: ''
+}, {
+    imagePath: basicImagePath + 'drop_down_list.png',
+    componentTitle: 'Dropdown',
+    name: ''
+}, {
+    imagePath: basicImagePath + 'button.jpg',
+    componentTitle: 'Button',
+    name: ''
+}];
+
+const componentLabelItems = arrOfComponentLabels.map( (item,index) =>
+    /*
+    Ideally some unique id should be used for key,
+    for the time being index has been used,
+    this should be replaced once stable Ids are available
+    */
+        <LeftPaneItem
+            key = { index }
+            imagePath = { item.imagePath }
+            componentTitle = { item.componentTitle }
+            name = { item.name } 
+        />
+    );
 export default class LeftPane extends React.Component {
 
     render() {
